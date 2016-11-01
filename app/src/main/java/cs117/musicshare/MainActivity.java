8ca119cf160902e90261e929cd9dbe8affa75faf
joshstclair <josh.jds@gmail.com>
2016-11-01 01:07:08 -0700

    Play music from list

--- app/src/main/java/cs117/musicshare/MainActivity.java
+++ app/src/main/java/cs117/musicshare/MainActivity.java
@@ -9,6 +9,13 @@ import android.net.Uri;
 import android.content.ContentResolver;
 import android.database.Cursor;
 import android.widget.ListView;
+import android.os.IBinder;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -35,15 +42,35 @@ public class MainActivity extends Activity {
         }
     }
 
+    public void songPicked(View view){
+        musicSrv.setSong(Integer.parseInt(view.getTag().toString()));
+        musicSrv.playSong();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_shuffle:
+                //shuffle
+                break;
+            case R.id.action_end:
+                stopService(playIntent);
+                musicSrv=null;
+                System.exit(0);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        songView = (ListView) findViewById(R.id.song_list);
+        songView = (ListView)findViewById(R.id.song_list);
         songList = new ArrayList<Song>();
         getSongList();
-        Collections.sort(songList, new Comparator<Song>() {
-            public int compare(Song a, Song b) {
+        Collections.sort(songList, new Comparator<Song>(){
+            public int compare(Song a, Song b){
                 return a.getTitle().compareTo(b.getTitle());
             }
         });
@@ -51,6 +78,42 @@ public class MainActivity extends Activity {
         songView.setAdapter(songAdt);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (playIntent == null) {
+            playIntent = new Intent(this, MusicService.class);
+            bindService(playIntent, musicConnection, Context.BIND_AUTO_CREATE);
+            startService(playIntent);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        stopService(playIntent);
+        musicSrv=null;
+        super.onDestroy();
+    }
+
+    private ServiceConnection musicConnection = new ServiceConnection(){
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MusicService.MusicBinder binder = (MusicService.MusicBinder)service;
+            musicSrv = binder.getService();
+            musicSrv.setList(songList);
+            musicBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            musicBound = false;
+        }
+    };
+
     private ArrayList<Song> songList;
     private ListView songView;
+    private MusicService musicSrv;
+    private Intent playIntent;
+    private boolean musicBound=false;
 }
---
+++ app/src/main/java/cs117/musicshare/MusicService.java
@@ -0,0 +1,103 @@
+package cs117.musicshare;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import java.util.ArrayList;
+import android.content.ContentUris;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.PowerManager;
+import android.util.Log;
+
+/**
+ * Created by joshua on 11/1/16.
+ */
+
+public class MusicService extends Service implements
+        MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener,
+        MediaPlayer.OnCompletionListener {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        songPosn=0;
+        player = new MediaPlayer();
+        initMusicPlayer();
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return musicBind;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent){
+        player.stop();
+        player.release();
+        return false;
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        mp.start();
+    }
+
+    public void initMusicPlayer(){
+        player.setWakeMode(getApplicationContext(),
+                PowerManager.PARTIAL_WAKE_LOCK);
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        player.setOnPreparedListener(this);
+        player.setOnCompletionListener(this);
+        player.setOnErrorListener(this);
+    }
+
+    public void playSong(){
+        player.reset();
+        Song playSong = songs.get(songPosn);
+        long currSong = playSong.getID();
+        Uri trackUri = ContentUris.withAppendedId(
+                android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                currSong);
+        try{
+            player.setDataSource(getApplicationContext(), trackUri);
+        }
+        catch(Exception e){
+            Log.e("MUSIC SERVICE", "Error setting data source", e);
+        }
+        player.prepareAsync();
+    }
+
+    public void setSong(int songIndex){
+        songPosn=songIndex;
+    }
+
+    public class MusicBinder extends Binder {
+        MusicService getService() {
+            return MusicService.this;
+        }
+    }
+
+    public void setList(ArrayList<Song> theSongs){
+        songs=theSongs;
+    }
+
+    private MediaPlayer player;
+    private ArrayList<Song> songs;
+    private int songPosn;
+    private final IBinder musicBind = new MusicBinder();
+
+}
