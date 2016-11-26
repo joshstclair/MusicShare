b0ad33453cf104677eb47756f2cc7ea6a47d1bda
akappy7 <akapbasov@gmail.com>
2016-11-26 10:01:06 -0800

    Adding initialization of wifi p2p

--- app/src/main/java/cs117/musicshare/RightFragment.java
+++ app/src/main/java/cs117/musicshare/RightFragment.java
@@ -12,32 +12,25 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBar;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Set;
-import java.util.UUID;
-import android.os.Handler;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.ActionListener;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.net.wifi.p2p.WifiP2pManager.ChannelListener;
+
+import static android.os.Looper.getMainLooper;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -57,8 +50,53 @@ public class RightFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_right,
                 container, false);
 
+        //setup peers list
+        peerList = (ListView) view.findViewById(R.id.PeersList);
+
+        //-------wifi direct init----
+        mManager = (WifiP2pManager) getActivity().getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(getActivity(), getMainLooper(), null);
+        mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
+
+        //init wifi direct intent-filters
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(getActivity(), "Discovery Initiated",
+                        Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onFailure(int reasonCode) {
+                Toast.makeText(getActivity(), "Discovery Failed : " + reasonCode,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        mManager.onPeersAvailable(myPeerListListener);
+        //Toast.makeText(getActivity(), "Peer list: " + myPeerListListener,
+                //Toast.LENGTH_SHORT).show();
+
+
         return view;
         //return inflater.inflate(R.layout.fragment_right, container, false);
+    }
+
+    /* register the broadcast receiver with the intent values to be matched */
+    @Override
+    public void onResume() {
+        super.onResume();
+        getActivity().registerReceiver(mReceiver, mIntentFilter);
+    }
+    /* unregister the broadcast receiver */
+    @Override
+    public void onPause() {
+        super.onPause();
+        getActivity().unregisterReceiver(mReceiver);
     }
 
     //Pop up a message to the user
@@ -72,6 +110,59 @@ public class RightFragment extends Fragment {
         super.onDestroy();
     }
 
+    //event handler for wifi direct
+    public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
+
+        private WifiP2pManager mManager;
+        private WifiP2pManager.Channel mChannel;
+        private RightFragment mActivity;
+
+        public WiFiDirectBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel,
+                                           RightFragment activity) {
+            super();
+            this.mManager = manager;
+            this.mChannel = channel;
+            this.mActivity = activity;
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+                // Check to see if Wi-Fi is enabled and notify appropriate activity
+                int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+                if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                    //wifi p2p is enabled
+                    show_Message("Wifi p2p is enabled");
+                } else {
+                    // Wi-Fi P2P is not enabled
+                    show_Message("Wifi p2p is not enabled!");
+                }
+            } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+                // Call WifiP2pManager.requestPeers() to get a list of current peers
+                // request available peers from the wifi p2p manager. This is an
+                // asynchronous call and the calling activity is notified with a
+                // callback on PeerListListener.onPeersAvailable()
+                if (mManager != null) {
+                    mManager.requestPeers(mChannel, myPeerListListener);
+                }
+            } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+                // Respond to new connection or disconnections
+            } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+                // Respond to this device's wifi state changing
+            }
+        }
+    }
+
     View view;
 
+    //intent filter for wifi direct
+    WifiP2pManager.PeerListListener myPeerListListener;
+    IntentFilter mIntentFilter;
+    WifiP2pManager mManager;
+    WifiP2pManager.Channel mChannel;
+    BroadcastReceiver mReceiver;
+
+    ListView peerList;
 }
